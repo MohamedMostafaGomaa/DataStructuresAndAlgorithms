ed184dc482f64160b0c4bc2837ee9cf5510fb557
Mohamed Mostafa Gomaa <Moh.MostafaGomaa@gmail.com>
2018-12-05 14:06:41 -0500

    Add project files.

---
+++ DataStructuresAndAlgorithms.Test/BinarySearchTreeTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using DataStructuresAndAlgorithms.Logic;
+using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.Test
+{
+    [TestFixture]
+    public class BinarySearchTreeTest
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 4, 1, 10)]
+        public void Test_CheckBST(int[] arr, int root, int min, int max)
+        {
+            #region preperation
+
+            List<Node> nodes = new List<Node>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Node node = new Node();
+                nodes.Add(new Node() { data = arr[i] });
+
+            }
+
+            for (int j = 0; j < nodes.Count; j++)
+            {
+                if (j == 0)
+                {
+                    nodes[j].left = null;
+                    nodes[j].right = nodes[j + 1];
+                }
+                else if (j >= nodes.Count - 1)
+                {
+                    nodes[j].left = nodes[j - 1];
+                    nodes[j].right = null;
+                }
+                else
+                {
+                    nodes[j].left = nodes[j - 1];
+                    nodes[j].right = nodes[j + 1];
+                }
+
+            }
+
+            #endregion
+            BinarySearchTree bst = new BinarySearchTree();
+
+            var result = bst.CheckBST(nodes[3]);
+
+            Assert.AreEqual(result, true);
+            
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataStructuresAndAlgorithms.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataStructuresAndAlgorithms.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26f9aab2-3bd1-4da6-b5b7-fc3809382823")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
